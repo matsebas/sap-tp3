5a862fd603565f3e88bc7c08ab93a76b28496c0d
Matias Sebastiao <msebastiao@max.capital>
2024-07-20 11:46:39 -0300

    ConsultarTurnosView

--- src/main/java/com/msebastiao/sap/SuperchargerApplication.java
+++ src/main/java/com/msebastiao/sap/SuperchargerApplication.java
@@ -34,6 +34,7 @@ public class SuperchargerApplication extends Application {
         solicitarTurnoItem.setOnAction(e -> openView("Clientes - Solicitar Turno", "SolicitarTurnoView"));
         clientesMenu.getItems().add(solicitarTurnoItem);
         MenuItem consultarTurnosItem = new MenuItem("Consultar Turnos");
+        solicitarTurnoItem.setOnAction(e -> openView("Clientes - Consultar Turnos", "ConsultarTurnosView"));
         clientesMenu.getItems().add(consultarTurnosItem);
 
         // Elementos para el menú Recepción
@@ -41,6 +42,7 @@ public class SuperchargerApplication extends Application {
         registrarAsistenciaItem.setOnAction(e -> openView("Recepción - Registrar Asistencia", "RegistrarAsistenciaView"));
         recepcionMenu.getItems().add(registrarAsistenciaItem);
         MenuItem gestionarTurnos = new MenuItem("Gestionar Turnos");
+        gestionarTurnos.setDisable(true);
         recepcionMenu.getItems().add(gestionarTurnos);
 
         // Elementos para el menú Taller
@@ -48,6 +50,7 @@ public class SuperchargerApplication extends Application {
         realizarMantenimientoItem.setOnAction(e -> openView("Taller - Realizar Mantenimiento", "RealizarMantenimientoView"));
         tallerMenu.getItems().add(realizarMantenimientoItem);
         MenuItem consultarTareas = new MenuItem("Consultar Tareas Pendientes");
+        consultarTareas.setDisable(true);
         tallerMenu.getItems().add(consultarTareas);
 
         // Elementos para el menú Operaciones
@@ -55,12 +58,16 @@ public class SuperchargerApplication extends Application {
         supervisarTareasItem.setOnAction(e -> openView("Operaciones - Supervisar Tareas", "SupervisarTareasView"));
         operacionesMenu.getItems().add(supervisarTareasItem);
         MenuItem generarReportes = new MenuItem("Generar Reportes");
+        generarReportes.setDisable(true);
         operacionesMenu.getItems().add(generarReportes);
 
         // Elementos para el menú Administración
         MenuItem gestionarConvenios = new MenuItem("Gestionar Convenios");
+        gestionarConvenios.setDisable(true);
         MenuItem configurarSistema = new MenuItem("Configurar Sistema");
+        configurarSistema.setDisable(true);
         MenuItem administrarUsuarios = new MenuItem("Administrar Usuarios");
+        administrarUsuarios.setDisable(true);
         MenuItem generarInformeItem = new MenuItem("Emitir Informes Mensuales");
         administracionMenu.getItems().addAll(gestionarConvenios, configurarSistema, administrarUsuarios);
         generarInformeItem.setOnAction(e -> openView("Administración - Emitir Informes Mensuales", "GenerarInformeMensualView"));
